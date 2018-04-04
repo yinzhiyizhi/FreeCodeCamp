b9b95207e2c59564d472463625e6a8ab359bd0d5
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-04-04 15:49:22 +0800

    大致弄好了，微博可以分享

--- Build a Random Quote Machine/Build a Random Quote Machine.js
+++ Build a Random Quote Machine/Build a Random Quote Machine.js
@@ -1,17 +1,22 @@
-$(document).ready(function(){
-    $(".btn_next").on("click",function(){
-        getQuote();
-    });
+$(document).ready(function () {
 
-    var content="";
-    var author="";
-    function getQuote(){
-        $.getJSON("https://v1.hitokoto.cn/",function(json){
-            content=json["hitokoto"];
-            author=json["from"];
+    $(".btn_next").on("click", getQuote);
+    var content = "Blessed is the man, who having nothing to say, abstains from giving wordy evidence of the fact.";
+    var author = "Paul Erdos";
+
+    function getQuote() {
+        $.getJSON("https://v1.hitokoto.cn/", function (json) {
+            content = json["hitokoto"];
+            author = json["from"];
 
             $(".quote_content").html(content);
-            $(".quote_author").html(author);
+            $(".quote_author").html("——" + author);
         });
     }
+    getQuote();
+    $(".shareweibo").on("click", function () {
+        console.log(content + author);
+        window.open("http://service.weibo.com/share/share.php?appkey=&title=" + content + "——" + author);
+    });
+
 });
