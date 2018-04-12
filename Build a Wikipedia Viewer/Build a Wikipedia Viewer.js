7033a3ab5f9d0eb83de319d34937a0be2c5901bb
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-04-12 21:47:00 +0800

    js

--- Build a Wikipedia Viewer/Build a Wikipedia Viewer.js
+++ Build a Wikipedia Viewer/Build a Wikipedia Viewer.js
@@ -1,39 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function () {
     wikipediaViewerMain();
 });
 
-function wikipediaViewerMain(){
-    var search=$(".search");
-    var randomUrl='https://en.wikipedia.org/wiki/Special:Random';
-    var searchUrl='https://en.wikipedia.org/w/api.php';
-    var searchValue='';
+function wikipediaViewerMain() {
+    var search = $('.search');
+    var result=$('.result');
+    var randomUrl = 'https://en.wikipedia.org/wiki/Special:Random';
+    var searchUrl = 'https://en.wikipedia.org/w/api.php';
+    var searchValue = '';
 
-    function getSearchBoxValue(){
-        if(search.value.length!==0){
-            searchValue=search.value;
-            searchValue=searchValue.replace(/(\r\n|\n\r)/gm,'');
-        }
-    }
-
-    $('#random-button').click(function(){
+    $('#random-button').click(function () {
         window.open(randomUrl);
     });
 
-    $('search-button').click(function(){
-        if(search.value.length!==0){
-            searchValue=search.value;
-            searchValue=searchValue.replace(/(\r\n|\n\r)/gm,'');
-        }
-        else{
-            search.value='';
+    $('#search-button').click(function () {
+        if (search.val() !== '') {
+            searchValue = search.val();
+            searchValue = searchValue.replace(/(\r\n|\n\r)/gm, '');
+        } else {
+            result.html('');
             exit;
         }
 
         $.ajax({
-            
+            url: searchUrl,
+            data: {
+                action: 'query',
+                list: 'search',
+                srsearch: searchValue,
+                format: 'json'
+            },
+            dataType: 'jsonp',
+            success: function (x) {
+                var html = '<!-- result -->';
+                html += '<div class="row row-centered">';
+
+                x.query.search.map(function (w) {
+                    html += '  <div class="col-xs-10 col-centered">';
+                    html += '    <a href="https://en.wikipedia.org/wiki/' + w.title + '" target="_blank">';
+                    html += '        <div class="panel panel-default">';
+                    html += '            <div class="panel-heading">';
+                    html += '                <h3 class="panel-title">' + w.title + '</h3>';
+                    html += '            </div>';
+                    html += '            <div class="panel-body">';
+                    html += '                ' + w.snippet;
+                    html += '            </div>';
+                    html += '        </div>';
+                    html += '    </a>';
+                    html += '  </div>';
+                });
+                html += '</div>';
+                result.html(html);
+            }
         });
     });
-
-
-
 }
