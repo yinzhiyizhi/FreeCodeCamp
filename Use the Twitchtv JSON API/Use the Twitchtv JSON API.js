9df6815c305c952c5ebd90d4416474956dd7bd41
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-04-16 21:42:41 +0800

    ...

--- Use the Twitchtv JSON API/Use the Twitchtv JSON API.js
+++ Use the Twitchtv JSON API/Use the Twitchtv JSON API.js
@@ -3,7 +3,15 @@ $(document).ready(function(){
 });
 
 var channels=[];
+var client_Id='';
 
 function getChannelInfo(){
-    
+    channels.forEach(function(channel){
+        function makeURL(type,name){
+            return 'https://api.twitch.tv/kraken/'+type+'/?client_id='+client_Id+'&'+name;
+        };
+
+        $.get
+
+    });
 }
