3929cf7ec6cd18e6159b3152d608286f72d4492e
yinzhiyizhi <yinzhiyizhi@gmail.com>
2018-04-21 21:06:21 +0800

    基本完成了

--- Use the Twitchtv JSON API/Use the Twitchtv JSON API.js
+++ Use the Twitchtv JSON API/Use the Twitchtv JSON API.js
@@ -3,47 +3,38 @@ $(document).ready(function () {
 });
 
 var channels = ["freecodecamp", "test_channel", "ESL_SC2"];
-var client_Id = 'i4suuyjog3z5kawqo9ygt4d0p8a7ub';
 
 function getChannelInfo() {
     channels.forEach(function (channel) {
-        function makeURL(name) {
-            return 'https://api.twitch.tv/kraken/streams/?client_id=' + client_Id + '&channel=' + name+'&callback=?';
+        function makeURL(type, name) {
+            return 'https://wind-bow.gomix.me/twitch-api/' + type + '/' + name + '?callback=?';
         };
 
-        $.getJSON(makeURL(channel), function (data) {
-            var twitchChannel='https://api.twitch.tv/kraken/channels/'+channel;
+        $.getJSON(makeURL("streams", channel), function (data) {
+            var game, status;
 
-            var game,status;
-
-            $.getJSON(twitchChannel).done(channel=>{
-                if(channeldata.status===404){
-                    game="Account Closed";
-                    status="offline";
-                }
-            });
-
-            
-            if(data.stream===null){
-                game="Offline";
-                status="offline";
-            }
-            else {
-                game = data.streams[0].channel.game;
+            if (data.stream === undefined) {
+                game = "Account Closed";
+                status = "offline";
+            } else if (data.stream === null) {
+                game = "Offline";
+                status = "offline";
+            } else {
+                game = data.stream.game;
                 status = "online";
             }
 
-            var data = data.streams[0].channel;
-            var logo = data.logo !== null ? data.logo : "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
-            var name = data.display_name !== null ? data.display_name : channel;
-            var description = status === "online" ? ":" + data.status : "";
+            $.getJSON(makeURL("channels", channel),function (data) {
+                var logo = data.logo !== null ? data.logo : "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
+                var name = data.display_name !== null ? data.display_name : channel;
+                var description = status === "online" ? ":" + data.status : "";
 
-            var html = '<div class="row"><div class="col-xs-2 col-sm-1"><img src="' + logo + '" alt="" class="logo"></div><div class="col-xs-10 col-sm-3"><a href="' + data.url + '" target="_blank">' + name + '</a></div>            <div class="col-xs-10 col-sm-8">' +
-                game + '<span>' + description + '</span></div></div>'
+                var html = '<div class="row ' + status + '"><div class="col-xs-2 col-sm-1" id="icon"><img src="' + logo + '" alt="" class="logo"></div><div class="col-xs-10 col-sm-3" id="name"><a href="' + data.url + '" target="_blank">' + name + '</a></div>            <div class="col-xs-10 col-sm-8" id="streaming">' +
+                    game + '<span class="hidden-xs">' + description + '</span></div></div>'
 
-            status === "online" ? $(".display").prepend(html) : $(".display").append(html);
+                status === "online" ? $(".display").prepend(html) : $(".display").append(html);
+            });
 
         });
-
     });
 }
